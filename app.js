1625a1661369d077517a0274f77cb7e1291b2dc1
Felipe Bona <felipechiodinibona@hotmail.com>
2021-02-13 23:37:48 -0300

    Adicionado Cors

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 3000
+
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+}
 
 const cities = require('./routes/citiesRouter')
 
+app.use(cors(corsOptions))
 app.use('/', cities)
 
 app.listen(port)
